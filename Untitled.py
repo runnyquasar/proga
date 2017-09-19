547511f7fa0e4d70b4e2eab01074f6b1e6742d68
Nefedova Aleksandra <31654688+runnyquasar@users.noreply.github.com>
2017-09-19 11:53:47 +0300

    1 seminar

---
+++ Untitled.py
@@ -0,0 +1,23 @@
+print("Hello")
+#x=10
+#y=x*1000
+#print("y=" ,y)
+#print('x=',x,"y=" ,y)
+
+#human_name=input("Как вас зовут? ")
+#print("Привет", human_name)
+
+#print(type(human_name))
+
+#count=input("Введите Х")
+#count2=input("Введите У")
+#num1=int(count)
+#um2=int(count2)
+#print(num1*num2)
+
+number=int(input("vvedite chislo:"))
+if number > 5:
+    print("vashe chislo bolssee 5")
+    print("!!")
+    
+s
