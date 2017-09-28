c4fe41f10491796773caa1d00adb5ba4bc94340f
Nefedova Aleksandra <31654688+runnyquasar@users.noreply.github.com>
2017-09-28 10:29:55 +0300

    first hw

---
+++ hw1/homework1.py
@@ -0,0 +1,15 @@
+x=int(input("Enter x:"))
+y=int(input("Enter y:"))
+z=int(input("Enter z:"))
+
+if x+y==z:
+    print("x+y=z")
+else:
+    print("not this day")
+      
+    if x/y==z:
+        print("x/y=z")
+    else:
+        print("=(")
+
+print("The end")
