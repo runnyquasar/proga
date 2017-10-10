0c84f788124f6c085291ba7e5a1a8cc322b921d6
Nefedova Aleksandra <31654688+runnyquasar@users.noreply.github.com>
2017-10-10 20:14:20 +0300

    hz

---
+++ Untitled2.py
@@ -0,0 +1,34 @@
+#word="Hi,Mark"
+#print(word)
+#my_sum=0
+#for i in range(3,20,2):
+#    my_sum+=i
+#    print(i,"my_sum =", my_sum)
+    
+#print (my_sum)
+
+#x=int(input("Set ur number"))
+#print(x)
+
+#num=0
+#for x in range (0, x+1):
+  #  num+=x
+ #   print(num, x)
+
+num=int(input("Numbers: "))
+for i in range(num):
+    x=int(input("Your number: "))
+    
+    if x>0:
+        print(">0")
+    elif x<0:
+        print("<0")
+    else:
+        print("0")
+
+    
+    
+
+
+print("The end.")
+ 
---
+++ hw2/hw2_v4.py
@@ -0,0 +1,9 @@
+num=int(input("Numbers: "))
+for i in range(num):
+    x=input("Введите слова: ")
+    print(x)
+
+    if x=="программирование" :
+        print(" Ваше слово - 'программирование'; программа завершает работу")
+        break
+print("Конец")
