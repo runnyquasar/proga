5107e4bf50de6a8d36528d0fbda848fcfe0c4e06
Nefedova Aleksandra <31654688+runnyquasar@users.noreply.github.com>
2017-12-07 11:54:55 +0300

    control3

--- control/exfor8points.py
+++ control/exfor8points.py
@@ -1,15 +1,13 @@
 lines=[]
 with open("/Users/andreakimov/Desktop/freq.txt", encoding="utf-8") as f:
     lines=f.readlines()
-    c=0
-    for line in lines:
-        
-        form=line.split('|')
-        
+    ipm=0
+    for line in lines:        
+        form=line.split('|')        
         if form[1]==' част ':
-            print(form[:1],',')
-            c+=float(form[2])
-    print(c)
+            print(form[:1],',',end=' ')
+            ipm+=float(form[2])
+    print(ipm)
  
             
             
