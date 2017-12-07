bd0f1c6ae7d707f06631e90a45f497ed41cf2662
Nefedova Aleksandra <31654688+runnyquasar@users.noreply.github.com>
2017-12-07 12:04:44 +0300

    cjntron

--- control/10points.py
+++ control/10points.py
@@ -1,13 +1,20 @@
 lines=[]
 with open("/Users/andreakimov/Desktop/freq.txt", encoding="utf-8") as f:
     lines=f.readlines()
-    user_word=input('Введите слово: ')
+ 
     for line in lines:
         form=line.split('|')
-        
+        user_word=input('Введите слово: ')
+        if user_word=='' :
+            break
+ 
         if user_word in lines:
             print(form[1:2])
         else:
-            print('в нашем словаре такого нет:(')
+            print('takogo net')
+            
+        
+       
+        
             
 
